0d2ac31b7cbd1b07aae43dde26c3f73047894fcd
nl-hugo <nl-hugo@hugojanssen.nl>
2018-07-31 16:52:18 +0200

    Add rest framework

--- firedept/settings.py
+++ firedept/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'polls.apps.PollsConfig',
+    'snippets.apps.SnippetsConfig',
+    'rest_framework',
     'firedept'
 ]
 
--- firedept/urls.py
+++ firedept/urls.py
@@ -4,4 +4,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^api-auth/', include('rest_framework.urls')),
+    url(r'^', include('snippets.urls')),
 ]
