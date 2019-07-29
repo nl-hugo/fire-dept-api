eff5e12591224358a63d2d74ae84c8e4268dd0c1
nl-hugo <nl-hugo@outlook.com>
2019-07-29 16:13:24 +0200

    Retain Maarn-Maarsbergen only

--- cleanup.py
+++ cleanup.py
@@ -1,6 +1,4 @@
 import os
-import json
-import requests
 import logging
 import django
 
@@ -20,100 +18,19 @@ django.setup()
 logger.info('Django version {}'.format(django.__version__))
 
 
-def cleanup(plaatsen):
-    from alarmeringen.models import Alarmering
-    capcodes = ['707507','707509','707510','707511','707515','707516']
+def cleanup():
+    from alarmeringen.models import Alarmering, CapCode
+    capcodes = ['707507', '707508', '707509', '707510', '707511', '707512', '707513', '707514', '707515', '707516']
 
-    n, objs = Alarmering.objects.filter(plaats__in=plaatsen).exclude(capcodes__in=capcodes).delete()
+    n, objs = Alarmering.objects.exclude(capcodes__in=capcodes).delete()
     logger.info('Deleted {} alarmeringen'.format(n))
+
+    n, objs = CapCode.objects.exclude(capcode__in=capcodes).delete()
+    logger.info('Deleted {} capcodes'.format(n))
 
 
 def run():
-    plaatsen = [
-        'Abcoude',
-        'Amersfoort',
-        'Amstelhoek',
-        'Baambrugge',
-        'Baarn',
-        'Barneveld',
-        'Bennekom',
-        'Blokker',
-        'Benschop',
-        'Bilthoven',
-        'Bosch en duin',
-        'Bussum',
-        'De bilt',
-        'De kwakel',
-        'De meern',
-        'Den dolder',
-        'Eemdijk',
-        'Eemnes',
-        'Everdingen',
-        'Gorinchem',
-        'Groenekan',
-        'Haarzuilens',
-        'Hagestein',
-        'Harmelen',
-        'Hekendorp',
-        'Hernen',
-        'Hoevelaken',
-        'Hooglanderveen',
-        'Houten',
-        'Huizen',
-        'Hilversum',
-        'Hollandsche rading',
-        'Hoogland',
-        'Kampen',
-        'Kamerik',
-        'Kedichem',
-        'Kortenhoef',
-        'Kudelstaart',
-        'Kockengen',
-        'Lage vuursche',
-        'Laren nh',
-        'Leerdam',
-        'Leusden',
-        'Loenersloot',
-        'Lexmond',
-        'Linschoten',
-        'Loenen aan de vecht',
-        'Lopik',
-        'Maarssen',
-        'Maartensdijk',
-        'Mijdrecht',
-        'Montfoort',
-        'Nederhorst den berg',
-        'Nederland',
-        'Neer',
-        'Nieuwegein',
-        'Nieuwerbrug',
-        'Nieuwersluis',
-        'Nigtevecht',
-        'Odijk',
-        'Oud zuilen',
-        'Ouderkerk aan de amstel',
-        'Oudewater',
-        'Schalkwijk',
-        'Soest',
-        'Soesterberg',
-        '\'t goy',
-        'Tienhoven ut',
-        'Uithoorn',
-        'Utrecht',
-        'Vianen ut',
-        'Vinkeveen',
-        'Vleuten',
-        'Vreeland',
-        'Waverveen',
-        'Wilnis',
-        'Woerden',
-        'Woerdense verlaat',
-        'Zegveld',
-        'Zijderveld',
-        'Zwartebroek',
-        ''
-    ]
-    cleanup(plaatsen)
+    cleanup()
 
 
 if __name__ == '__main__':
